c255f00fcf55e595401982f336b34f17c41cffc5
Prince Chaurasia <prncchaurasia@gmail.com>
2022-09-07 20:57:50 +0530

    Add files via upload

--- module4/script.js
+++ module4/script.js
@@ -1,21 +1,21 @@
-var names=new Array();
-names[0]="Yaakov";
-names[1]="John";
-names[2]="Jen";
-names[3]="jason";
-names[4]="paul";
-names[5]="frank";
-names[6]="larry";
-names[7]="paula";
-names[8]="laura";
-names[9]="jim";
+var names1=new Array();
+names1[0]="Yaifkov";
+names1[1]="Josshn";
+names1[2]="Jsen";
+names1[3]="jasofn";
+names1[4]="pausl";
+names1[5]="fratnk";
+names1[6]="lartry";
+names1[7]="paudla";
+names1[8]="ladura";
+names1[9]="jimd";
 
 
-for (var i = 0; i < names.length; i++) {
-	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
-        console.log("Goodbye "+ names[i])
+for (var i = 0; i < names1.length; i++) {
+	if(names1[i].charAt(0)==='J'|| names1[i].charAt(0)==='j'){
+        console.log("Goodbye "+ names1[i])
 	}
 	else{
-		console.log("Hello "+ names[i])
+		console.log("Hello "+ names1[i])
 	}
 }
